55f661496beb740286260c97b7fa9a837eec9988
pshenix <pshenixx@gmail.com>
2025-11-05 15:01:25 +0000

    add middle version of ui tests (with page object pattern)

--- src/tests/api/create_account_test.py
+++ src/tests/api/create_account_test.py
@@ -1,7 +1,7 @@
 import pytest
 
-from src.main.classes.api_manager import ApiManager
-from src.main.models.create_user_request import CreateUserRequest
+from src.main.api.classes.api_manager import ApiManager
+from src.main.api.models.create_user_request import CreateUserRequest
 
 
 @pytest.mark.api
--- src/tests/ui/base_test.py
+++ src/tests/ui/base_test.py
@@ -1,5 +1,15 @@
-from src.main.configs.config import Config
+from playwright.sync_api import Page
+
+from src.main.api.configs.config import Config
+from src.main.api.specs.request_specs import RequestSpecs
+from src.main.api.models.create_user_request import CreateUserRequest
 
 
 class BaseUITest:
     UI_BASE_URL = Config.get("UI_BASE_URL", "http://localhost:3000")
+
+    def auth_as_user(self, page: Page, user_request: CreateUserRequest) -> None:
+        auth_token = RequestSpecs.auth_as_user(user_request.username, user_request.password).get("Authorization")
+        page.set_viewport_size({"width": 1920, "height": 1080})
+        page.goto(self.UI_BASE_URL)
+        page.evaluate('token => localStorage.setItem("authToken", token)', auth_token)
