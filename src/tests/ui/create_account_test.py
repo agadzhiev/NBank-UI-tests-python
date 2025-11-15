f369d874ce00fc94e6f843d0b7c56bfef3a01098
nobugs-me <alexpshe.help@gmail.com>
2025-11-15 19:10:49 +0000

    update

--- src/tests/ui/create_account_test.py
+++ src/tests/ui/create_account_test.py
@@ -1,22 +1,22 @@
 import pytest
 from playwright.sync_api import Page, expect
 
-from src.main.ui.pages.bank_alert import BankAlert
 from src.main.api.classes.api_manager import ApiManager
-from src.main.ui.pages.user_dashboard import UserDashboard
 from src.main.api.models.create_user_request import CreateUserRequest
+from src.main.ui.pages.user_dashboard import UserDashboard
+from src.main.ui.pages.bank_alert import BankAlert
 
 
 @pytest.mark.ui
 class TestCreateAccount:
-    @pytest.mark.user_session(1)
-    @pytest.mark.usefixtures("user_request", "api_manager")
-    def test_user_can_create_account(self, page: Page, api_manager: ApiManager, user_request: CreateUserRequest):
-        dashboard_page = UserDashboard(page).open()\
-            .create_new_account()\
-            .check_alert_message_and_accept(BankAlert.NEW_ACCOUNT_CREATED)
+    @pytest.mark.user_session(10)
+    def test_user_can_create_account(self, api_manager: ApiManager, page: Page, user_request: CreateUserRequest):
+        dashboard_page = UserDashboard(page).open()
         expect(dashboard_page.welcome_text).to_be_visible()
+        dashboard_page = dashboard_page.create_new_account()
+        dashboard_page.check_alert_message_and_accept(BankAlert.NEW_ACCOUNT_CREATED)
+        
+        user_accounts = api_manager.user_steps.get_all_accounts(user_request)
 
-        created_accounts = api_manager.user_steps.get_all_accounts(user_request)
-        assert len(created_accounts) == 1
-        assert created_accounts[0] and created_accounts[0].balance == 0
+        assert len(user_accounts) == 1
+        assert user_accounts[0] and user_accounts[0].balance == 0
--- src/tests/ui/create_user_test.py
+++ src/tests/ui/create_user_test.py
@@ -1,33 +1,35 @@
+import re
 import pytest
 from playwright.sync_api import Page, expect
 
-from src.main.api.models.role import Role
-from src.main.ui.pages.bank_alert import BankAlert
-from src.main.ui.pages.admin_panel import AdminPanel
+from src.main.api.models.create_user_request import CreateUserRequest
 from src.main.api.classes.api_manager import ApiManager
 from src.main.api.generators.random_data import RandomData
-from src.main.api.models.create_user_request import CreateUserRequest
-from src.main.api.models.comparison.model_assertions import models_match
 from src.main.api.generators.random_model_generator import RandomModelGenerator
+from src.main.api.models.comparison.model_assertions import ModelAssertions
+from src.main.api.models.role import Role
+from src.main.ui.pages.admin_panel import AdminPanel
+from src.main.ui.pages.bank_alert import BankAlert
 
 
 @pytest.mark.ui
 class TestCreateUser:
     @pytest.mark.admin_session
     @pytest.mark.parametrize('new_user_request', [RandomModelGenerator.generate(CreateUserRequest)])
-    def test_admin_can_create_user(self, page: Page, api_manager: ApiManager, new_user_request: CreateUserRequest):
+    def test_admin_can_create_user(self, page: Page, api_manager: ApiManager, new_user_request: CreateUserRequest):     
         api_manager.admin_steps.created_objects.append(new_user_request)
-        
-        admin_page = AdminPanel(page).open()\
-            .create_user(new_user_request.username, new_user_request.password)\
-            .check_alert_message_and_accept(BankAlert.USER_CREATED_SUCCESSFULLY)\
-            .wait_for_username(new_user_request.username)
-        
-        expect(admin_page.admin_panel_text).to_be_visible()
-        assert any(u.username == new_user_request.username for u in admin_page.get_all_users())
 
-        created_user = next(u for u in api_manager.admin_steps.get_all_users() if u.username == new_user_request.username)
-        models_match(created_user, new_user_request)
+        admin_page = AdminPanel(page).open()
+        expect(admin_page.admin_panel_text).to_be_visible()
+        admin_page.create_user(new_user_request.username, new_user_request.password)
+        admin_page.check_alert_message_and_accept(BankAlert.USER_CREATED_SUCCESSFULLY)
+        admin_page.wait_for_username(new_user_request.username)
+
+        created_user = next(
+            u for u in api_manager.admin_steps.get_all_users()
+            if u.username == new_user_request.username
+        )
+        ModelAssertions(created_user, new_user_request).match()
 
     @pytest.mark.admin_session
     @pytest.mark.parametrize(
@@ -35,10 +37,12 @@ class TestCreateUser:
         [CreateUserRequest(username=RandomData.get_username(1), password=RandomData.get_password(), role=Role.USER)]
     )
     def test_admin_cannot_create_user_with_invalid_data(self, page: Page, api_manager: ApiManager, new_user_request: CreateUserRequest):
-        admin_page = AdminPanel(page).open()\
-            .create_user(new_user_request.username, new_user_request.password)\
-            .check_alert_message_and_accept(BankAlert.USERNAME_MUST_BE_BETWEEN_3_AND_15_CHARACTERS)
-        
+        api_manager.admin_steps.created_objects.append(new_user_request)
+
+        admin_page = AdminPanel(page).open()
         expect(admin_page.admin_panel_text).to_be_visible()
+
+        admin_page = admin_page.create_user(new_user_request.username, new_user_request.password)
+        admin_page.check_alert_message_and_accept(BankAlert.USERNAME_MUST_BE_BETWEEN_3_AND_15_CHARACTERS)
         assert not any(u.username == new_user_request.username for u in admin_page.get_all_users())
         assert not any(u.username == new_user_request.username for u in api_manager.admin_steps.get_all_users())
